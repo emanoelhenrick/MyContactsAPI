ee3ec109b2971a59c0f27a88cc4aa5c103f00d0d
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-02-16 10:23:16 -0300

    add error handling

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import routes from "./routes";
+import "express-async-errors";
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.log(err);
+  res.sendStatus(500);
+});
 
 app.listen(3000, () => {
   console.log("listening on port 8080");
